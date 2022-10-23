46335172a31c2045572adf4ff3cc33b02e9054c6
thinhle <52267526+fizzbb3102@users.noreply.github.com>
2022-10-24 01:51:29 +0700

    Create UPController

---
+++ UserProfileController.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.AccountDAO;
+import entity.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UserProfileController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UserProfileController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UserProfileController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Account currentUser = (Account) session.getAttribute("currentUser");
+        if (currentUser != null) {
+            AccountDAO accDb = new AccountDAO();
+            Account info = accDb.getAccountInfo(currentUser);
+            request.setAttribute("info", info);
+            request.getRequestDispatcher("user_profile.jsp").forward(request, response);
+        } else {
+            request.setAttribute("check", true);
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            int aid = Integer.parseInt(request.getParameter("aid"));
+            String email = request.getParameter("email");
+            String fullname = request.getParameter("fullname");
+            int phone = Integer.parseInt(request.getParameter("phone"));
+            
+            Account ai = new Account();
+            ai.setAid(aid);
+            ai.setEmail(email);
+            ai.setFullname(fullname);
+            ai.setPhone(phone);
+            
+            AccountDAO accDb = new AccountDAO();
+            accDb.updateAccountInfo(ai);
+            response.sendRedirect("/WebApplication2/profile?success=1");
+        } catch (Exception e) {
+            response.sendRedirect("/WebApplication2/profile?success=0");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
